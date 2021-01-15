dcff5d9d4113cbf416912074f63d10c29f660a09
Adriana Santibañez <adrianasanben@gmail.com>
2021-01-14 23:29:57 -0600

    Plot3

--- Plot3.R
+++ Plot3.R
@@ -15,8 +15,8 @@ datetime <- strptime(datetimepaste, format = "%Y-%m-%d %H:%M:%S")
 ESM$Date <- datetime
 
 plot(ESM$Date,ESM$Sub_metering_1, type="l", ylab="Energy Submetering", xlab="")
-lines(ESM$Sub_metering_2, type = "l", col="blue")
-lines(ESM$Sub_metering_3, type = "l", col="red")
+lines(ESM$Date,ESM$Sub_metering_2, type ="l", col="blue")
+lines(ESM$Date, ESM$Sub_metering_3, type ="l", col="red")
 legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c("black","red","blue"),
        lty = 1, lwd = 2.5)
 dev.off()
